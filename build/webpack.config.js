56bc013b6af03feffd31f8004400cf15a18b9977
wang_meng <wang.m.bj>
2019-07-12 00:45:58 +0800

    clean-webpack-plugin 插件失效

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -8,7 +8,7 @@ function resolve(_dir){
     return path.join(__dirname, '..', _dir);
 }
 
-const config = {
+const webpackConfig = {
     entry: resolve('src/main.js'),
     resolve: {
         extensions: ['.js', '.vue', '.json'],
@@ -49,4 +49,4 @@ const config = {
     ]
 }
 
-module.exports = config;
+module.exports = webpackConfig;
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
